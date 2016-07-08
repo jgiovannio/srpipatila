e7bc8354154147e5320c7ed4847fcedfcf732fb9
Raul Caraballo <raul.caraballo.5@gmail.com>
2016-07-08 14:47:35 -0500

    adjust test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,13 +4,13 @@ describe Juego do
 
   it "Jugador selecciona Papel y maquina escoje Piedra" do
     jugador = Juego.new
-    jugador.seleccionar "Papel"
+    jugador.seleccionar "Papel", 2
     jugador.resultado.should == "Ganas"
   end
 
   it "Jugador selecciona Piedra y maquina escoje Piedra" do
     jugador = Juego.new
-    jugador.seleccionar "Piedra"
+    jugador.seleccionar "Piedra", 2
     jugador.resultado.should == "Empate"
   end
 
