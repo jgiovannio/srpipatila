14636ca58447f1f8e2f03b9dced38e63208d022e
Raul Caraballo <raul.caraballo.5@gmail.com>
2016-07-08 13:08:11 -0500

    Arreglo

--- lib/srpipatila.rb
+++ lib/srpipatila.rb
@@ -1,6 +1,8 @@
 class Juego
 
-  OBJETOS = ["Papel" , "Piedra" ]
+  #OBJETOS_USUARIO = {"Papel" => 1 , "Piedra" => 3 , "Tijera" => 2 }
+  OBJETOS_MAQUINA = ["Papel", "Piedra" , "Tijera"]
+  OBJETOS_USUARIO = {1 => "Papel", 3 => "Piedra" , 2 => "Tijera" }
 
   def initialize
     @seleccion = ""
@@ -11,11 +13,18 @@ class Juego
   end
 
   def resultado
-    if @seleccion == OBJETOS[0]
-      return "Ganas"
-    else
+    maquina = Random.new
+    resultado = maquina.rand(1..3).to_i
+    if OBJETOS_MAQUINA[maquina.rand(0..2).to_i] == @seleccion
       return "Empate"
+    elsif condition
+
     end
+
+    #if OBJETOS_USUARIO[@seleccion] == maquina
+      #return OBJETOS_USUARIO[@seleccion]
+    #else
+    #end
   end
 
 end
